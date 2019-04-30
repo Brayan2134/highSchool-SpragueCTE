6e56703ededc0bc655e369dcc28b1c1fade356a1
BrayanQ <brayanquevedo2134@gmail.com>
2019-04-29 17:15:34 -0700

    Basic Page Structure Has Been Set.
    All of the required files have been made, but have no contents. All header information and meta data of files are created.

---
+++ js/navBar.js
@@ -0,0 +1,22 @@
+/**
+ * Author: Quevedo Ramos, Brayan
+ * Co-Author: Millet, Jordan
+ * 
+ * Date: 4/29/19
+ * 
+ * Purpose:
+ *          - This is the page where the navbar will be displayed on the page.
+ *          - Place this at the top of the body of the html page.  
+ *          - Code as normal Nav bar and make sure to add \ at the end of each line.    
+ *          - Code will be pasted into the page.
+ *      
+ * General Notes:
+ *          - Make sure to check the <base href="" /> and make sure that it goes to the 
+ *            root dir, so it can work on any page.
+ *          - If adding to nav later, pretend that your coding from the root folder.
+ */
+
+document.write('');
+
+// Debugging Statement
+console.log('The navigation bar has been loaded.');
