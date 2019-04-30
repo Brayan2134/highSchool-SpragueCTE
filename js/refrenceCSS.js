381ba82a8e0c1fd7396d2ae80f19c0d2cda8696f
BrayanQ <brayanquevedo2134@gmail.com>
2019-04-29 17:15:34 -0700

    [master] | Basic Page Structure Has Been Set.
    All of the required files have been made, but have no contents. All header information and meta data of files are created.

---
+++ js/refrenceCSS.js
@@ -0,0 +1,40 @@
+/**
+ * Author: Quevedo Ramos, Brayan
+ * Co-Author: Millet, Jordan
+ * 
+ * Date: 4/29/19
+ * 
+ * Purpose:
+ *          - This is the refrence point for all CSS on page.
+ *          - Place this at the head of the html page.  
+ *          - Code as normal Nav bar and make sure to add \ at the end of each line.    
+ *          - Code will be pasted into the page.
+ *      
+ * General Notes:
+ *          - Make sure to check the <base href="" /> and make sure that it goes to the 
+ *            root dir, so it can work on any page.
+ *          - If adding to this file later, pretend that your coding from the root folder.
+ *          - MAKE SURE THAT YOUR NOT ADDING ONE PAGE CSS,
+ *            ADD ONE PAGE CSS ON HEAD OF SPECIFIC FILE!!!
+ */
+
+// Sample File
+// <link rel="stylesheet" href="#">
+ 
+document.write('\
+    <!--Global CSS-->\
+    <link rel="stylesheet" href="css/global.css">\
+\
+    <!--Header CSS-->\
+    <link rel="stylesheet" href="css/header">\
+\
+    <!--Sidebar CSS-->\
+    <link rel="stylesheet" href="css/sidebar.css">\
+\
+    <!--Footer CSS-->\
+    <link rel="stylesheet" href="#">\
+\
+');
+
+// Debugging Statement
+console.log('All CSS files have been loaded');
